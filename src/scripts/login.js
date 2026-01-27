fcbf487c185285093c52b5b94dfe5726ae74d30b
purrluto <purrluto@gmail.com>
2026-01-26 22:01:18 -0600

    starting on login (don't do it like this) (very bad)

---
+++ src/scripts/login.js
@@ -0,0 +1,12 @@
+// Made w/ jquery.
+// No this isn't secure but it's okay because it's for a goofy little site.
+// Don't put your passwords into JS...
+
+$(document).ready(function () {
+    let username = $('#user').val();
+    let password = $('#pass').val();
+});
+
+$("form").submit(function(){
+  alert("Submitted");
+});
